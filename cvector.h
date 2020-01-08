6af5aa9b0c9ba12d5991a51cb01ae4a585391551
Lam Wei Lun <weilunlam1996@gmail.com>
2020-01-08 14:37:30 +0800

    Update cvector.h
    Prepend data members with an underscore.

--- cvector.h
+++ cvector.h
@@ -6,65 +6,65 @@
 #define CVECTOR(TYPE) \
 struct \
 { \
-  TYPE *data; \
-  unsigned int size; \
-  unsigned int max_size;\
+  TYPE* _data; \
+  unsigned int _size; \
+  unsigned int _capacity;\
 }
 
-#define CVECTOR_AT(VEC, INDEX) VEC.data[INDEX]
+#define CVECTOR_AT(VEC, INDEX) VEC._data[INDEX]
 
-#define CVECTOR_BACK(VEC) VEC.data[VEC.size - 1]
+#define CVECTOR_BACK(VEC) VEC._data[VEC._size - 1]
 
 #define CVECTOR_CLEAR(VEC) \
 { \
-  VEC.size = 0; \
-  VEC.max_size = 0; \
-  free(VEC.data); \
+  VEC._size = 0; \
+  VEC._capacity = 0; \
+  free(VEC._data); \
 }
 
-#define CVECTOR_DATA(VEC) VEC.data;
+#define CVECTOR__data(VEC) VEC._data;
 
-#define CVECTOR_EMPTY(VEC) VEC.size == 0
+#define CVECTOR_EMPTY(VEC) VEC._size == 0
 
-#define CVECTOR_FRONT(VEC) VEC.data[0];
+#define CVECTOR_FRONT(VEC) VEC._data[0];
 
 #define CVECTOR_INIT(VEC, CAP) \
 { \
-  VEC.data = malloc(CAP * sizeof(*(VEC).data)); \
-  VEC.size = 0; \
-  VEC.max_size = CAP; \
+  VEC._data = malloc(CAP * sizeof(*(VEC)._data)); \
+  VEC._size = 0; \
+  VEC._capacity = CAP; \
 }
 
-#define CVECTOR_CAPACITY(VEC) VEC.max_size
+#define CVECTOR_CAPACITY(VEC) VEC.max__size
 
-#define CVECTOR_EMPTY(VEC) VEC.size == 0
+#define CVECTOR_EMPTY(VEC) VEC._size == 0
 
-#define CVECTOR_POPBACK(VEC) --VEC.size
+#define CVECTOR_POPBACK(VEC) --VEC._size
 
 #define CVECTOR_PUSHBACK(VEC, ELEMENT) \
 { \
-  if (VEC.size + 1 == VEC.max_size) \
+  if (VEC._size + 1 == VEC._capacity) \
   { \
-    VEC.max_size *= 2; \
-    VEC.data = realloc(VEC.data, VEC.max_size * sizeof(*(VEC).data)); \
+    VEC._capacity *= 2; \
+    VEC._data = realloc(VEC._data, VEC._capacity * sizeof(*(VEC)._data)); \
   } \
-  VEC.data[VEC.size] = ELEMENT; \
-  ++VEC.size; \
+  VEC._data[VEC._size] = ELEMENT; \
+  ++VEC._size; \
 }
 
-#define CVECTOR_SIZE(VEC) VEC.size
+#define CVECTOR__size(VEC) VEC._size
 
 #define CVECTOR_SWAP(VEC1, VEC2) \
 { \
-  void* tmp = VEC1.data; \
-  unsigned int tmpSize = VEC1.size; \
-  unsigned int tmpMaxSize = VEC1.max_size; \
-  VEC1.data = VEC2.data; \
-  VEC1.size = VEC2.size; \
-  VEC1.max_size = VEC2.max_size; \
-  VEC2.data = tmp; \
-  VEC2.size = tmpSize; \
-  VEC2.max_size = tmpMaxSize; \
+  void* tmp = VEC1._data; \
+  unsigned int tmp_size = VEC1._size; \
+  unsigned int tmpMax_size = VEC1._capacity; \
+  VEC1._data = VEC2._data; \
+  VEC1._size = VEC2._size; \
+  VEC1._capacity = VEC2._capacity; \
+  VEC2._data = tmp; \
+  VEC2._size = tmp_size; \
+  VEC2._capacity = tmpMax_size; \
 }
 
 #endif
